4c1461b66eb9a68579c8ffa38bbdfefdf6bc3cdc
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-11 20:51:25 +0100

    Minor update

--- src/SB/UserBundle/Controller/RegistrationController.php
+++ src/SB/UserBundle/Controller/RegistrationController.php
@@ -7,6 +7,7 @@ use SB\UserBundle\Form\Type\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Date;
 
 class RegistrationController extends Controller
 {
@@ -27,6 +28,7 @@ class RegistrationController extends Controller
             // 3) Encode the password (you could also do this via Doctrine listener)
             $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
+            $user->setSalt(md5(new Date()));
 
             // 4) save the User!
             $em = $this->getDoctrine()->getManager();
