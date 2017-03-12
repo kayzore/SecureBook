b8654ea468074c17846f9d41929197cb948a89dc
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-12 12:20:19 +0100

    Check si l'utilisateur est déjà connecté sur la page d'accueil

--- src/SB/CoreBundle/Controller/CoreController.php
+++ src/SB/CoreBundle/Controller/CoreController.php
@@ -8,6 +8,9 @@ class CoreController extends Controller
 {
     public function indexAction()
     {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+
+        }
         return $this->render('SBCoreBundle:Home:index.html.twig');
     }
 }
