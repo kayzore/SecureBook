7e3d77f40960f1e64030ab3629897a835de116b6
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-12 16:07:08 +0100

    Ajout du syteme d'amitié

--- src/SB/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/SB/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -5,12 +5,13 @@ namespace SB\UserBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use SB\UserBundle\Entity\User;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadUser extends AbstractFixture implements FixtureInterface, ContainerAwareInterface
+class LoadUser extends AbstractFixture implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
 {
     /**
      * @var ContainerInterface
@@ -25,23 +26,30 @@ class LoadUser extends AbstractFixture implements FixtureInterface, ContainerAwa
     public function load(ObjectManager $manager)
     {
         // Les noms d'utilisateurs à créer
-        $listNames = array('Kayzore', 'Alexandre', 'Marine', 'Anna');
+        $listNames = array(
+            array('Kayzore', array(1, 2)),
+            array('Alexandre', array(0, 2, 3)),
+            array('Marine', array(0, 1, 3)),
+            array('Anna', array(0, 1, 2)),
+        );
 
-        foreach ($listNames as $key => $name) {
+        foreach ($listNames as $key => $new_user) {
             // On crée l'utilisateur
             $user = new User;
 
             // Le nom d'utilisateur et le mot de passe sont identiques
-            $user->setUsername($name);
+            $user->setUsername($new_user[0]);
             $encoder = $this->container->get('security.password_encoder');
-            $password = $encoder->encodePassword($user, $name);
+            $password = $encoder->encodePassword($user, $new_user[0]);
             $user->setPassword($password);
-            $user->setEmail($name . '@fakemail.com');
+            $user->setEmail($new_user[0] . '@fakemail.com');
 
             // On ne se sert pas du sel pour l'instant
             $user->setSalt('');
             // On définit uniquement le role ROLE_USER qui est le role de base
             $user->setRoles(array('ROLE_USER'));
+
+            $user->setFriends($new_user[1]);
 
             // On le persiste
             $manager->persist($user);
@@ -50,5 +58,10 @@ class LoadUser extends AbstractFixture implements FixtureInterface, ContainerAwa
 
         // On déclenche l'enregistrement
         $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 1;
     }
 }
