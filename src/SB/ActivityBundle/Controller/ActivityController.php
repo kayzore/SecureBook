4d37582739050312755f8bfd7bc301f33088c7ce
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-19 22:49:17 +0100

    Séparation du core controller et des activités

---
+++ src/SB/ActivityBundle/Controller/ActivityController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace SB\ActivityBundle\Controller;
+
+use SB\ActivityBundle\Entity\Activity;
+use SB\ActivityBundle\Entity\Comment;
+use SB\ActivityBundle\Entity\Likes;
+use SB\ActivityBundle\Form\Type\ActivityType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class ActivityController extends Controller
+{
+    public function activityAddAction(Request $request)
+    {
+        $activity = new Activity();
+        $form = $this->createForm(new ActivityType(), $activity);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $activity->setUser($this->getUser());
+
+            if (!is_null($activity->getImage()->getFile())) {
+                $activity->getImage()->upload();
+            } else {
+                $activity->setImage(null);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($activity);
+            $em->flush();
+            return $this->redirectToRoute('sb_core_homepage');
+        }
+        return $this->createAccessDeniedException('Acces Denied');
+    }
+
+    public function activityLikeAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $id_activity = $request->request->get('id_activity');
+            $user = $this->getUser();
+
+            $em = $this->getDoctrine()->getManager();
+            $activity = $em->getRepository('SBActivityBundle:Activity')->findOneBy(array('id' => $id_activity));
+
+            $like = $em->getRepository('SBActivityBundle:Likes')->findOneBy(array(
+                'user' => $user,
+                'activity' => $activity
+            ));
+
+            if (is_null($like)) {
+                $activity->addOneLike();
+
+                $likes = new Likes();
+                $likes->setActivity($activity);
+                $likes->setUser($user);
+
+                $em->persist($likes);
+                $em->persist($activity);
+                $em->flush();
+            }
+            $activity_likes = $activity->getNbLikes();
+            return new JsonResponse(array('activity_likes' => $activity_likes));
+        }
+        return $this->createAccessDeniedException('Acces Denied');
+    }
+
+    public function activityDislikeAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $id_activity = $request->request->get('id_activity');
+            $user = $this->getUser();
+
+            $em = $this->getDoctrine()->getManager();
+            $activity = $em->getRepository('SBActivityBundle:Activity')->findOneBy(array('id' => $id_activity));
+
+            $like = $em->getRepository('SBActivityBundle:Likes')->findOneBy(array(
+                'user' => $user,
+                'activity' => $activity
+            ));
+
+            if (!is_null($like)) {
+                $activity->removeOneLike();
+
+                $em->remove($like);
+                $em->persist($activity);
+                $em->flush();
+            }
+            $activity_likes = $activity->getNbLikes();
+            return new JsonResponse(array('activity_likes' => $activity_likes));
+        }
+        return $this->createAccessDeniedException('Acces Denied');
+    }
+
+    public function activityCommentAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $comment_text = $request->request->get('comment_text');
+            $activity_id = $request->request->get('id_activity');
+            $user = $this->getUser();
+
+            $em = $this->getDoctrine()->getManager();
+            $activity = $em->getRepository('SBActivityBundle:Activity')->findOneBy(array('id' => $activity_id));
+
+            $activity->addOneComment();
+
+            $comment = new Comment();
+            $comment->setActivity($activity);
+            $comment->setUser($user);
+            $comment->setText($comment_text);
+
+            $em->persist($activity);
+            $em->persist($comment);
+            $em->flush();
+
+            $activity_comments = $activity->getNbComments();
+            return new JsonResponse(array('activity_comments' => $activity_comments));
+        }
+        return $this->createAccessDeniedException('Acces Denied');
+    }
+
+    public function activityGetCommentAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $activity_id = $request->request->get('id_activity');
+
+            $em = $this->getDoctrine()->getManager();
+            $comments = $em->getRepository('SBActivityBundle:Comment')->fetchAll($activity_id, 3);
+
+            return $this->render('SBActivityBundle:comments:comment.html.twig', array(
+                'comments' => $comments
+            ));
+        }
+        return $this->createAccessDeniedException('Acces Denied');
+    }
+}
