e628b66e5750498bfe556a6e5251cb669f6f313e
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-25 18:29:51 +0100

    Create likes system file

--- web/assets/app/js/activity.js
+++ web/assets/app/js/activity.js
@@ -54,54 +54,12 @@ $(document).ready(function () {
     $(document).on('click', '.btn-like', function(){
         var btn = this,
             id_activity = this.parentNode.parentNode.parentNode.dataset.activity;
-        $(btn).append('<i class="fa fa-spinner fa-pulse fa-fw"></i>');
-        $.ajax({
-            url: Routing.generate('sb_activity_like'),
-            method: 'post',
-            data: {id_activity: id_activity},
-            dataType: 'json',
-            success: function (result) {
-                var likes_text;
-                if (result.activity_likes > 2) {
-                    likes_text = result.activity_likes + " J'aimes";
-                } else {
-                    likes_text = result.activity_likes + " J'aime";
-                }
-                $(btn)
-                    .html(likes_text)
-                    .removeClass('btn-like')
-                    .removeClass('btn-primary')
-                    .addClass('btn-dislike')
-                    .addClass('btn-success')
-                ;
-            }
-        });
+        likes.addLike(btn, id_activity);
     });
     $(document).on('click', '.btn-dislike', function(){
         var btn = this,
             id_activity = this.parentNode.parentNode.parentNode.dataset.activity;
-        $(btn).append('<i class="fa fa-spinner fa-pulse fa-fw"></i>');
-        $.ajax({
-            url: Routing.generate('sb_activity_dislike'),
-            method: 'post',
-            data: {id_activity: id_activity},
-            dataType: 'json',
-            success: function (result) {
-                var likes_text;
-                if (result.activity_likes > 2) {
-                    likes_text = result.activity_likes + " J'aimes";
-                } else {
-                    likes_text = result.activity_likes + " J'aime";
-                }
-                $(btn)
-                    .html(likes_text)
-                    .removeClass('btn-dislike')
-                    .removeClass('btn-success')
-                    .addClass('btn-like')
-                    .addClass('btn-primary')
-                ;
-            }
-        });
+        likes.removeLike(btn, id_activity);
     });
 
     $(document).on('click', '.btn-show-comments', function () {
---
+++ web/assets/app/js/likes.js
@@ -0,0 +1,69 @@
+var likes = function () {
+    var addLike,
+        removeLike;
+
+    /**
+     * Add like of activity
+     * @param btn
+     * @param id_activity
+     */
+    addLike = function (btn, id_activity) {
+        $(btn).append('<i class="fa fa-spinner fa-pulse fa-fw"></i>');
+        $.ajax({
+            url: Routing.generate('sb_activity_like'),
+            method: 'post',
+            data: {id_activity: id_activity},
+            dataType: 'json',
+            success: function (result) {
+                var likes_text;
+                if (result.activity_likes > 2) {
+                    likes_text = result.activity_likes + " J'aimes";
+                } else {
+                    likes_text = result.activity_likes + " J'aime";
+                }
+                $(btn)
+                    .html(likes_text)
+                    .removeClass('btn-like')
+                    .removeClass('btn-primary')
+                    .addClass('btn-dislike')
+                    .addClass('btn-success')
+                ;
+            }
+        });
+    };
+
+    /**
+     * Remove like of activity
+     * @param btn
+     * @param id_activity
+     */
+    removeLike = function (btn, id_activity) {
+        $(btn).append('<i class="fa fa-spinner fa-pulse fa-fw"></i>');
+        $.ajax({
+            url: Routing.generate('sb_activity_dislike'),
+            method: 'post',
+            data: {id_activity: id_activity},
+            dataType: 'json',
+            success: function (result) {
+                var likes_text;
+                if (result.activity_likes > 2) {
+                    likes_text = result.activity_likes + " J'aimes";
+                } else {
+                    likes_text = result.activity_likes + " J'aime";
+                }
+                $(btn)
+                    .html(likes_text)
+                    .removeClass('btn-dislike')
+                    .removeClass('btn-success')
+                    .addClass('btn-like')
+                    .addClass('btn-primary')
+                ;
+            }
+        });
+    };
+
+    return {
+        addLike: addLike,
+        removeLike: removeLike
+    };
+}();
