8c388eeab917d76ad9a410097d984f4100d56477
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-25 20:20:03 +0100

    Refactoring load more activities system

--- web/assets/app/js/activities.js
+++ web/assets/app/js/activities.js
@@ -1,13 +1,114 @@
 var activities = function () {
     var autogrow_add_activity,
         form_add_preview_image,
-        init_activities;
+        init_activities,
+        loadMoreActivities,
+        getMoreActivities,
+        showNewActivities,
+        new_waypoint,
+        waypoint,
+        loading_activity = false,
+        all_activity_loaded = false,
+        container_activities,
+        activities_list,
+        current_page;
 
     /**
      * Init activities functionality
      */
     init_activities = function () {
         autogrow_add_activity();
+    };
+
+    /**
+     * System to load more activities
+     * @param page
+     * @param activityList
+     * @param container_activity
+     */
+    loadMoreActivities = function (page, activityList, container_activity) {
+        var lastActivity = activityList.last(),
+            id_last_activity = lastActivity[0].dataset.activity,
+            route;
+
+        current_page = page;
+        activities_list = activityList;
+        container_activities = container_activity;
+
+        if (!loading_activity && !all_activity_loaded) {
+            loading_activity = true;
+            container_activities.fadeIn('slow').append(''
+                + '<div class="col-md-8" id="loading-activity">'
+                    + '<p class="text-center">Chargement en cours <i class="fa fa-spinner fa-pulse fa-fw"></i></p>'
+                + '</div>'
+            );
+
+            if (page == 'accueil') {
+                route = Routing.generate('sb_activity_get_activity');
+                getMoreActivities(route, id_last_activity);
+            } else if (page == 'profil') {
+                route = Routing.generate('sb_activity_get_my_activity');
+                getMoreActivities(route, id_last_activity);
+            }
+        }
+    };
+
+    /**
+     * Start ajax request to get more activities
+     * @param route
+     * @param id_last_activity
+     */
+    getMoreActivities = function (route, id_last_activity) {
+        $.ajax({
+            url: route,
+            method: 'post',
+            data: {id_last_activity: id_last_activity},
+            dataType: 'html',
+            success: function (list_activity) {
+                showNewActivities(list_activity);
+            }
+        });
+    };
+
+    /**
+     * Display new activities
+     * @param list_new_activities
+     */
+    showNewActivities = function (list_new_activities) {
+        $('#loading-activity').fadeOut('slow').remove();
+        container_activities.append(list_new_activities);
+        loading_activity = false;
+        if (container_activities[0].dataset.totalAtivity == $('#container-activity div.activity').length) {
+            waypoint.destroy();
+            all_activity_loaded = true;
+            container_activities.fadeIn('slow').append(''
+                + '<div class="col-md-8 no-pl no-pr">'
+                    + '<div class="alert alert-info alert-no-activity">'
+                        + '<p class="text-center">Plus d\'actualité</p>'
+                    + '</div>'
+                + '</div>'
+            );
+        } else {
+            waypoint.destroy();
+            activities_list = $('#container-activity div.activity');
+            new_waypoint($(activities_list[activities_list.length - 4]), current_page, activities_list, container_activities);
+        }
+    };
+
+    /**
+     * Set new waypoint
+     * @param activity
+     * @param page
+     * @param activityList
+     * @param container_activity
+     */
+    new_waypoint = function (activity, page, activityList, container_activity) {
+        waypoint = new Waypoint({
+            element: activity,
+            handler: function() {
+                loadMoreActivities(page, activityList, container_activity);
+            }
+        });
     };
 
     /**
@@ -39,6 +140,8 @@ var activities = function () {
     return {
         init_activities: init_activities,
         autogrow_add_activity: autogrow_add_activity,
-        form_add_preview_image: form_add_preview_image
+        form_add_preview_image: form_add_preview_image,
+        loadMoreActivities: loadMoreActivities,
+        new_waypoint: new_waypoint
     };
 }();
--- web/assets/app/js/all-activity.js
+++ web/assets/app/js/all-activity.js
@@ -1,58 +1,9 @@
 $(document).ready(function () {
-    var loading_activity = false,
-        all_activity_loaded = false,
-        container_activity = $('#container-activity'),
-        activityList = $('#container-activity div.activity'),
-        waypoint;
+    var container_activity = $('#container-activity'),
+        activityList;
 
     if (container_activity[0].dataset.totalAtivity > 5) {
-        waypoint = new Waypoint({
-            element: $(activityList[activityList.length - 4]),
-            handler: function() {
-                loadMoreActivity()
-            }
-        })
-    }
-
-    function loadMoreActivity() {
-        var activityList = $('#container-activity div.activity'),
-            lastActivity = activityList.last(),
-            id_last_activity = lastActivity[0].dataset.activity;
-        if (!loading_activity && !all_activity_loaded) {
-            loading_activity = true;
-            container_activity.fadeIn('slow').append(''
-                + '<div class="col-md-8" id="loading-activity">'
-                + '<p class="text-center">Chargement en cours <i class="fa fa-spinner fa-pulse fa-fw"></i></p>'
-                + '</div>');
-            $.ajax({
-                url: Routing.generate('sb_activity_get_activity'),
-                method: 'post',
-                data: {id_last_activity: id_last_activity},
-                dataType: 'html',
-                success: function (list_activity) {
-                    $('#loading-activity').fadeOut('slow').remove();
-                    container_activity.append(list_activity);
-                    loading_activity = false;
-                    if (container_activity[0].dataset.totalAtivity == $('#container-activity div.activity').length) {
-                        waypoint.destroy();
-                        all_activity_loaded = true;
-                        container_activity.fadeIn('slow').append(''
-                            + '<div class="col-md-8 no-pl no-pr">'
-                            + '<div class="alert alert-info alert-no-activity">'
-                            + '<p class="text-center">Plus d\'actualité</p>'
-                            + '</div>'
-                            + '</div>');
-                    } else {
-                        waypoint.destroy();
-                        waypoint = new Waypoint({
-                            element: $(activityList[activityList.length - 4]),
-                            handler: function() {
-                                loadMoreActivity()
-                            }
-                        });
-                    }
-                }
-            });
-        }
+        activityList = $('#container-activity div.activity');
+        activities.new_waypoint($(activityList[activityList.length - 4]), 'accueil', activityList, container_activity);
     }
 });
